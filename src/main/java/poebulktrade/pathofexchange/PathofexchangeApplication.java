32017939d32c094212b9682db346fb2924c9a269
asaaev <niaava1@gmail.com>
2024-01-25 18:25:59 +0200

    Initial commit

---
+++ src/main/java/poebulktrade/pathofexchange/PathofexchangeApplication.java
@@ -0,0 +1,13 @@
+package poebulktrade.pathofexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathofexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathofexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poebulktrade/pathofexchange/PathofexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package poebulktrade.pathofexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathofexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
